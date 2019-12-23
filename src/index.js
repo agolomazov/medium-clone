a71d87e9f2a940f71e905f8a32f50800ee466922
Anton Golomazov <golomazov-tosha2012@yandex.ru>
2019-12-24 02:21:55 +0300

    Init project

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Routes from 'routes';
+import { TopBar } from 'components/top-bar';
+import {CurrentUserProvider} from 'contexts/currentUser'
+
+const App = () => (
+  <CurrentUserProvider>
+    <Router>
+      <TopBar />
+      <Routes />
+    </Router>
+  </CurrentUserProvider>
+)
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
